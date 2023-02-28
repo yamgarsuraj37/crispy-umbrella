cce5d98bb81de591d9ec7ea24c2c9d2be156fce9
yamgarsuraj37 <yamgarsuraj37@gmail.com>
2023-02-28 11:25:59 +0530

    first commit n push

---
+++ src/com/p1/ProgramName1.java
@@ -0,0 +1,36 @@
+package com.p1;
+
+public class ProgramName1 {
+
+
+
+		public static void main(String[] args) {
+			//object class equals method : doing Reference comparison
+			
+
+			//String class overriding equals method : doing content(data)comparison 
+			String str1= "omkar";
+			String str2= "omkar";
+
+			System.out.println(str1.equals(str2));//true
+			System.out.println(str1==str2);       //true
+
+			String s1 = new String("omkar");
+			String s2 = new String("omkar");
+
+			System.out.println(s1.equals(s2));//true
+			System.out.println(s1==s2);       //false
+
+			//string buffer class can not overriding equals method of object class : it uses object class equals method
+			StringBuffer sb1 = new StringBuffer("omkar");
+			StringBuffer sb2 = new StringBuffer("omkar");
+
+			System.out.println(sb1.equals(sb2));//false
+			System.out.println(sb1==sb2);//false //false
+
+
+		}
+
+	}
+
+
